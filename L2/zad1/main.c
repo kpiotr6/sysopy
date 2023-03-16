3d618dec5c48e075949429157d2ed56d0321d387
kpiotr6 <piotrek@komlan.pl>
2023-03-16 22:03:38 +0100

    al3

--- L2/zad1/main.c
+++ L2/zad1/main.c
@@ -4,22 +4,49 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/stat.h>
+#include<time.h>
+
 
 #define BUFFER 1024
 
+#ifdef SYS
 void rewrite(char* find,char* change,char* from,char* to){
     char buff[1024];
     int from_fd = open(from,O_RDONLY);
     int to_fd = open(to,O_WRONLY|O_CREAT);
     int read_ctr;
     while(read_ctr = read(from_fd,buff,BUFFER)){
-        for(int i=0;i<BUFFER;i++){
-            
+        for(int i=0;i<strlen(buff);i++){
+            if(buff[i]==*find){
+                buff[i]=*change;
+            }
         }
         write(to_fd,buff,read_ctr);
     }
+
+    close(from_fd);
+    close(to_fd);
     chmod(to,S_IRWXU|S_IRWXG|S_IRWXO);
 }
+#else
+void rewrite(char* find,char* change,char* from,char* to){
+    char buff[1024];
+    FILE* from_fd = fopen(from,"r");
+    FILE* to_fd = fopen(to,"w");
+    int read_ctr;
+    while(read_ctr = fread(buff,sizeof(char),BUFFER,from_fd)){
+        for(int i=0;i<strlen(buff);i++){
+            if(buff[i]==*find){
+                buff[i]=*change;
+            }
+        }
+        fwrite(buff,sizeof(char),read_ctr,to_fd);
+    }
+    fclose(from_fd);
+    fclose(to_fd);
+    chmod(to,S_IRWXU|S_IRWXG|S_IRWXO);
+}
+#endif
 
 int main(int argc,char *argv[]){
     if(argc<4){
@@ -29,7 +56,14 @@ int main(int argc,char *argv[]){
     if(strlen(argv[1])>1 || strlen(argv[2])>1){
         fprintf(stderr,"Arg1 and Arg2 arent single characters.\n");
         return 1;
-    }  
-    rewrite(argv[1],argv[2],argv[3],argv[4]); 
+    }
+    clock_t p = clock();  
+    rewrite(argv[1],argv[2],argv[3],argv[4]);
+    #ifdef SYS
+    printf("SYS CPU time: %ld\n",clock()-p);
+    #else
+    printf("LIB CPU time: %ld\n",clock()-p);
+    #endif
+
     return 0;
 }
---
+++ L2/zad2/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define BUFFER 1024
+
+void reverse_one(char* from,char* to){
+    FILE* f = fopen(from,"rb");
+    FILE* t = fopen(to,"wb");
+    char c[1];
+    fseek(f,1,SEEK_END);
+    while(!fseek(f,-2,SEEK_CUR)){
+        fread(c,sizeof(char),1,f);
+        fwrite(c,sizeof(char),1,t);
+    }
+    fclose(f);
+    fclose(t);
+}
+
+void flip(char* to_flip,int size){
+    char b;
+    for(int i=0;i<size/2;i++){
+        b = to_flip[i];
+        to_flip[i] = to_flip[size-i-1];
+        to_flip[size-i-1] = b;
+    }
+}
+
+
+void reverse_buff(char* from,char* to){
+    FILE* f = fopen(from,"rb");
+    FILE* t = fopen(to,"wb");
+    char buff[BUFFER];
+    int rest = ftell(f);
+    fseek(f,0,SEEK_END);
+    rest = ftell(f);
+    while(!fseek(f,-1024,SEEK_CUR)){
+        rest = ftell(f);
+        fread(buff,sizeof(char),BUFFER,f);
+        flip(buff,BUFFER);
+        fwrite(buff,sizeof(char),BUFFER,t);
+        fseek(f,-1024,SEEK_CUR);
+    }
+    rest = ftell(f);
+    if(rest>0){
+        fseek(f,0,SEEK_SET);
+        fread(buff,sizeof(char),rest,f);
+        flip(buff,rest);
+        fwrite(buff,sizeof(char),rest,t);
+    }
+    fclose(f);
+    fclose(t);
+}
+
+int main(int argc,char *argv[]){
+    clock_t o,b;
+    o = clock();
+    reverse_one(argv[1],argv[2]);
+    printf("One CPU time: %ld\n",clock()-o);
+    b = clock();
+    reverse_buff(argv[1],strcat(argv[2],"_buff"));
+    printf("Buffer CPU time: %ld\n",clock()-b);
+    // printf("%f",3/5);
+    return 0;
+}
